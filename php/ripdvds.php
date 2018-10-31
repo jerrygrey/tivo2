f5425279f825ea98b4f46851377d28cfee597927
Jerry <jerrygrey@users.noreply.github.com>
2018-10-31 23:59:07 +1000

    Update ripdvds.php

--- php/ripdvds.php
+++ php/ripdvds.php
@@ -2,9 +2,13 @@
 
 define('NEW_LINES', ["\r\n", "\n\r", "\n", "\r"]);
 
-define('HANDBRAKE', 'C:\TiVo2\HandBrake.exe -i "%s" -o "D:\Temp\%s-%d.m4v" -e x265 --min-duration 1200 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 2.5 --keep-display-aspect --native-language eng --native-dub');
+define('HANDBRAKE', 'C:\TiVo2\HandBrake.exe -i "%s" -o "%s" -e x265 --min-duration 1200 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 2.5 --keep-display-aspect --native-language eng --native-dub');
+define('CSCRIPT', '%windir%\SysWoW64\cscript /nologo "%s"');
 
-$drives = shell_exec('%windir%\SysWoW64\cscript /nologo "C:\TiVo2\scripts\vbscripts\listdrives.vbs"');
+define('DIR_SCRIPTS', 'C:\TiVo2\scripts\vbscripts');
+define('DIR_WORKING', 'D:\Working\');
+
+$drives = shell_exec(sprintf(CSCRIPT, DIRECTORY.'\listdrives.vbs');
 
 $drives = trim($drives);
 
@@ -31,15 +35,26 @@ foreach ($drives as $letter) {
 	$label = trim($label[0]);
 	
 	$label = substr($label, 21);
+	
+	if (file_exists(DIR_WORKING.$label)) {
+		
+		$counter = 1;
+		
+		while (file_exists(DIR_WORKING.$label.$counter)) {
+			$counter++;
+		}
+		
+		$label = $label.$counter;
+		
+	}
+	
+	mkdir(DIR_WORKING.$label);
+	
 	var_dump($label);exit;
-	if ($label === NO_DISC) {
-		continue;
-	}
-	
 	for ($i = 0; $i <= 10; $i++) {
-		shell_exec(sprintf(HANDBRAKE, $letter, $label, $i));
+		shell_exec(sprintf(HANDBRAKE, $letter, DIR_WORKING.$label.'\'.$i.'.m4v'));
 	}
 	
-	shell_exec('%windir%\SysWoW64\cscript /nologo "C:\TiVo2\scripts\vbscripts\ejectdisc.vbs" {$letter}');
+	shell_exec(sprintf(CSCRIPT, DIRECTORY.'\ejectdisc.vbs').' '.$letter);
 	
 }
