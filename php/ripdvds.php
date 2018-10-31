834aef4cd5dd9a4ac6ca9e399e07f10c1afe09a8
Jerry <jerrygrey@users.noreply.github.com>
2018-10-31 21:56:02 +1000

    Update ripdvds.php

--- php/ripdvds.php
+++ php/ripdvds.php
@@ -4,7 +4,7 @@ define('NO_DISC', 'the device is not ready.');
 
 define('HANDBRAKE', 'C:\TiVo2\HandBrake.exe -i "%s" -o "D:\Temp\%s-%d.m4v" -e x265 --min-duration 1200 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 2.5 --keep-display-aspect --native-language eng --native-dub');
 
-$drives = `%windir%\SysWoW64\cscript /nologo "C:\TiVo2\scripts\vbscripts\listdrives.vbs"`;
+$drives = shell_exec('%windir%\SysWoW64\cscript /nologo "C:\TiVo2\scripts\vbscripts\listdrives.vbs"');
 var_dump($drives);exit;
 $drives = trim($drives);
 
@@ -28,6 +28,6 @@ foreach ($drives as $letter) {
 		shell_exec(sprintf(HANDBRAKE, $letter, $label, $i));
 	}
 	
-	`%windir%\SysWoW64\cscript /nologo "C:\TiVo2\scripts\vbscripts\ejectdisc.vbs" {$letter}`;
+	shell_exec('%windir%\SysWoW64\cscript /nologo "C:\TiVo2\scripts\vbscripts\ejectdisc.vbs" {$letter}');
 	
 }
