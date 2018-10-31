6eecfe4cb9ecacf861cf8be0164bd3bb062a9098
Jerry <jerrygrey@users.noreply.github.com>
2018-10-31 21:35:15 +1000

    Update ripdvds.php

--- php/ripdvds.php
+++ php/ripdvds.php
@@ -5,7 +5,7 @@ define('NO_DISC', 'the device is not ready.');
 define('HANDBRAKE', 'D:\scripts\HandBrake.exe -i "%s" -o "D:\Temp\%s-%d.m4v" -e x265 --min-duration 1200 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 2.5 --keep-display-aspect --native-language eng --native-dub');
 
 $drives = `%windir%\SysWoW64\vbscript /nologo "D:\Scripts\vbscripts\listdrives.vbs"`;
-
+var_dump($drives);exit;
 $drives = trim($drives);
 
 $drives = explode(PHP_EOL, $optical_drives);
