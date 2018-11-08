5c5aa1a2f25c12c80ac80c6d3b073c10ea5c5530
Jerry <jerrygrey@users.noreply.github.com>
2018-11-08 23:38:05 +1000

    Update startup.bat

--- batch/startup.bat
+++ batch/startup.bat
@@ -3,13 +3,13 @@ SetLocal EnableDelayedExpansion
 
 :check
 
-ping -n 31 127.0.0.1 > nul
-
-C:\TiVo2\php\php.exe C:\TiVo2\scripts\php\ripdvds.php
-
 ping -n 6 127.0.0.1 > nul
 
 cd C:\TiVo2\scripts
 git pull origin master
+
+ping -n 6 127.0.0.1 > nul
+
+C:\TiVo2\php\php.exe C:\TiVo2\scripts\php\ripdvds.php
 
 goto check
