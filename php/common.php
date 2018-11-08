de370660d70960294317d7477de43961b61da0de
Jerry <jerrygrey@users.noreply.github.com>
2018-11-08 13:37:09 +1000

    Update common.php

--- php/common.php
+++ php/common.php
@@ -1,18 +1,18 @@
 function scan_directory ( $item ) {
 	
-  if (is_dir($item)) {
+	if (is_dir($item)) {
 		
 		$array = [];
-    
-    $files = glob($item.'*', GLOB_MARK);
-    
-    foreach ($files as $file) {
-      $array = $array + scan($file);
-    }
+		
+		$files = glob($item.'*', GLOB_MARK);
+		
+		foreach ($files as $file) {
+			$array = $array + scan_directory($file);
+		}
 		
 		return $array;
-   	
-  }
+		
+	}
 	
 	return $item;
 	
