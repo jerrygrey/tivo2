620438f8426907a82703263df37306c4fe2b6cf4
Jerry <jerrygrey@users.noreply.github.com>
2018-11-08 15:10:31 +1000

    Create fromfolder.php

---
+++ php/fromfolder.php
@@ -0,0 +1,34 @@
+<?php
+
+define('HANDBRAKE', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" -o "%s.m4v" --main-feature -e x265 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 2.5 --keep-display-aspect --native-language eng --native-dub');
+
+define('DIR_AUTOMATIC', 'D:'.DIRECTORY_SEPARATOR.'Automatic'.DIRECTORY_SEPARATOR);
+define('DIR_WORKING', 'D:'.DIRECTORY_SEPARATOR.'Working'.DIRECTORY_SEPARATOR);
+
+$directory = new RecursiveDirectoryIterator(DIR_AUTOMATIC, RecursiveDirectoryIterator::SKIP_DOTS);
+$files = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($files as $file) {
+	
+	$directory = DIR_WORKING.'Automatic';
+	$file = DIRECTORY_SEPARATOR.$file;
+	
+	if (file_exists($directory)) {
+		
+		$counter = 1;
+		
+		while (file_exists($directory.$counter)) {
+			$counter++;
+		}
+		
+		$directory = $directory.$counter;
+		
+	}
+	
+	mkdir($directory);
+	
+	rename(DIR_AUTOMATIC.$file, $directory.$file);
+	
+	shell_exec(sprintf(HANDBRAKE, DIR_AUTOMATIC.$file, $directory.$file));
+	
+}
