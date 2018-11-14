109d5256d15ecfeac34a33314a97131d29b79fd1
Jerry <jerrygrey@users.noreply.github.com>
2018-11-15 02:32:33 +1000

    Update fromfolder.php

--- php/fromfolder.php
+++ php/fromfolder.php
@@ -1,9 +1,6 @@
 <?php
 
-define('HANDBRAKE', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" -o "%s.m4v" --main-feature -e x265 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 2.5 --keep-display-aspect --native-language eng --native-dub');
-
-define('DIR_AUTOMATIC', 'D:'.DIRECTORY_SEPARATOR.'Automatic'.DIRECTORY_SEPARATOR);
-define('DIR_WORKING', 'D:'.DIRECTORY_SEPARATOR.'Working'.DIRECTORY_SEPARATOR);
+require 'common.php';
 
 try {
 	
@@ -30,28 +27,14 @@ foreach ($files as $file) {
 	
 	$output = implode('', $output);
 	
-	$output = preg_replace('#[^a-z0-9]+#is', '', $output);
+	$directory = file_clearance($output);
 	
-	$directory = DIR_WORKING.$output;
+	mkdir(DIR_WORKING.$directory);
 	
-	if (file_exists($directory)) {
-		
-		$counter = 1;
-		
-		while (file_exists($directory.$counter)) {
-			$counter++;
-		}
-		
-		$directory = $directory.$counter;
-		
-	}
-	
-	mkdir($directory);
-	
-	$directory = $directory.DIRECTORY_SEPARATOR;
+	$directory = DIR_WORKING.$directory.DIRECTORY_SEPARATOR;
 	
 	rename($where.$file, $directory.$file);
 	
-	shell_exec(sprintf(HANDBRAKE, $directory.$file, $directory.$output));
+	shell_exec(sprintf(HANDBRAKE_FILE, $directory.$file, $directory.$output));
 	
 }
