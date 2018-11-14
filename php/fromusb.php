51719ebfac38c22ac9f45984bab27b7fc5eb9305
Jerry <jerrygrey@users.noreply.github.com>
2018-11-14 19:18:57 +1000

    Update fromusb.php

--- php/fromusb.php
+++ php/fromusb.php
@@ -1,5 +1,6 @@
 <?php
 
+define('NEW_LINES', ["\r\n", "\n\r", "\n", "\r"]);
 define('FILE_FORMATS', ['avi','mp4','m4v','mpg','mov']);
 
 define('HANDBRAKE', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" -o "%s.m4v" --main-feature -e x265 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 2.5 --keep-display-aspect --native-language eng --native-dub');
@@ -7,37 +8,49 @@ define('HANDBRAKE', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbr
 define('DIR_AUTOMATIC', 'D:'.DIRECTORY_SEPARATOR.'Automatic'.DIRECTORY_SEPARATOR);
 define('DIR_WORKING', 'D:'.DIRECTORY_SEPARATOR.'Working'.DIRECTORY_SEPARATOR);
 
-$drives = shell_exec('devcon find *USB*');
+$drives = shell_exec('wmic logicaldisk get caption');
+
+$drives = strtolower($drives);
+$drives = substr($drives, 7);
+$drives = trim($drives);
+
+$drives = str_replace(NEW_LINES, "\n", $drives);
+
+$drives = explode("\n", $drives);
+
+$drives = array_diff($drives, ['c:']);
+
+var_dump($drives);exit;
+
+foreach ($drives as $drive) {
 
 
-
-
-
-
-$directory = new RecursiveDirectoryIterator(DIR_AUTOMATIC, RecursiveDirectoryIterator::SKIP_DOTS);
-$files = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::SELF_FIRST);
-
-foreach ($files as $file) {
+	$directory = new RecursiveDirectoryIterator(DIR_AUTOMATIC, RecursiveDirectoryIterator::SKIP_DOTS);
+	$files = new RecursiveIteratorIterator($directory, RecursiveIteratorIterator::SELF_FIRST);
 	
-	$directory = DIR_WORKING.'Automatic';
-	$file = DIRECTORY_SEPARATOR.$file;
-	
-	if (file_exists($directory)) {
+	foreach ($files as $file) {
 		
-		$counter = 1;
+		$directory = DIR_WORKING.'Automatic';
+		$file = DIRECTORY_SEPARATOR.$file;
 		
-		while (file_exists($directory.$counter)) {
-			$counter++;
+		if (file_exists($directory)) {
+			
+			$counter = 1;
+		
+			while (file_exists($directory.$counter)) {
+				$counter++;
+			}
+			
+			$directory = $directory.$counter;
+			
 		}
 		
-		$directory = $directory.$counter;
+		mkdir($directory);
+		
+		rename(DIR_AUTOMATIC.$file, $directory.$file);
+		
+		shell_exec(sprintf(HANDBRAKE, DIR_AUTOMATIC.$file, $directory.$file));
 		
 	}
-	
-	mkdir($directory);
-	
-	rename(DIR_AUTOMATIC.$file, $directory.$file);
-	
-	shell_exec(sprintf(HANDBRAKE, DIR_AUTOMATIC.$file, $directory.$file));
 	
 }
