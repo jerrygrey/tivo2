ae3e5df8b9ab542eb31bcd309e1d7fdd6e378cd9
Jerry <jerrygrey@users.noreply.github.com>
2018-11-15 23:14:43 +1000

    Update startup.bat

--- batch/startup.bat
+++ batch/startup.bat
@@ -10,6 +10,8 @@ git pull origin master
 
 ping -n 6 127.0.0.1 > nul
 
-C:\TiVo2\php\php.exe C:\TiVo2\scripts\php\ripdvds.php
+C:\TiVo2\php\php.exe C:\TiVo2\scripts\php\fromdrives.php
+C:\TiVo2\php\php.exe C:\TiVo2\scripts\php\fromdvds.php
+C:\TiVo2\php\php.exe C:\TiVo2\scripts\php\fromfolder.php
 
 goto check
