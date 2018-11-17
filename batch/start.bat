a19abad4a2c210cb1c8efaa12419132422fdfea2
Jerry <jerrygrey@users.noreply.github.com>
2018-11-17 20:45:22 +1000

    Update start.bat

--- batch/start.bat
+++ batch/start.bat
@@ -3,9 +3,4 @@ SetLocal EnableDelayedExpansion
 
 ping -n 31 127.0.0.1 >nul
 
-cd C:\TiVo2\scripts
-git pull origin master >nul 2>&1
-
-ping -n 6 127.0.0.1 >nul
-
-start "TiVo2" /D "C:\TiVo2\scripts" /MAX loop.bat 
+start "TiVo2" /D "C:\TiVo2\scripts" /MAX loop.bat
