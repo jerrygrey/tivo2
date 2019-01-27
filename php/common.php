bdcff68f26d295ecd677200eb47639f0bd1717e1
Jerry <jerrygrey@users.noreply.github.com>
2019-01-27 21:53:28 +1000

    Update common.php

--- php/common.php
+++ php/common.php
@@ -9,9 +9,9 @@ define('DIR_TEMPORARY', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'sc
 define('DIR_AUTOMATIC', 'D:'.DIRECTORY_SEPARATOR.'Automatic'.DIRECTORY_SEPARATOR);
 define('DIR_WORKING', 'D:'.DIRECTORY_SEPARATOR.'Working'.DIRECTORY_SEPARATOR);
 
-define('HANDBRAKE_FILE', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" -o "%s.m4v" --main-feature -e x265 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 1.5 --keep-display-aspect --native-language eng --native-dub 2>&1');
-define('HANDBRAKE_SCAN', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" --title 0 -e x265 --min-duration 1200 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 1.5 --keep-display-aspect --native-language eng --native-dub 2>&1');
-define('HANDBRAKE_DVD', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" -o "%s.m4v" --title %d -e x265 --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 1.5 --keep-display-aspect --native-language eng --native-dub 2>&1');
+define('HANDBRAKE_FILE', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" -o "%s.m4v" --main-feature -e x265 --no-dvdnav --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 1.5 --keep-display-aspect --native-language eng --native-dub 2>&1');
+define('HANDBRAKE_SCAN', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" --title 0 -e x265 --min-duration 1200 --no-dvdnav --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 1.5 --keep-display-aspect --native-language eng --native-dub 2>&1');
+define('HANDBRAKE_DVD', 'C:'.DIRECTORY_SEPARATOR.'TiVo2'.DIRECTORY_SEPARATOR.'handbrake'.DIRECTORY_SEPARATOR.'handbrake.exe -i "%s" -o "%s.m4v" --title %d -e x265 --no-dvdnav --two-pass --audio-lang-list eng --first-audio --normalize-mix 1 --drc 1.5 --keep-display-aspect --native-language eng --native-dub 2>&1');
 
 define('CSCRIPT', 'C:'.DIRECTORY_SEPARATOR.'Windows'.DIRECTORY_SEPARATOR.'SysWoW64'.DIRECTORY_SEPARATOR.'cscript /nologo "%s"');
 
