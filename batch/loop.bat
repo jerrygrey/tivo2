b20be07bc475bd2f516070972afe443f0cf926e5
Jerry <jerrygrey@users.noreply.github.com>
2019-08-22 01:10:08 +1000

    Update loop.bat

--- batch/loop.bat
+++ batch/loop.bat
@@ -6,10 +6,12 @@ SetLocal EnableDelayedExpansion
 cd D:\TiVo2\scripts
 git pull origin master >nul 2>&1
 
-ping -n 6 127.0.0.1 >nul
+ping -n 36 127.0.0.1 >nul
 
 D:\TiVo2\php\php.exe D:\TiVo2\scripts\php\fromdrives.php
 D:\TiVo2\php\php.exe D:\TiVo2\scripts\php\fromdvds.php
 D:\TiVo2\php\php.exe D:\TiVo2\scripts\php\fromfolder.php
+
+ping -n 6 127.0.0.1 >nul
 
 goto loop
