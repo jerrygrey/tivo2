6d5b3bd884462eab6329ebc4ec5d4ee8ff4907b6
Jerry <jerrygrey@users.noreply.github.com>
2020-11-26 02:08:53 +1000

    Update loop.bat

--- batch/loop.bat
+++ batch/loop.bat
@@ -12,8 +12,9 @@ ping -n 36 127.0.0.1 >nul
 
 D:\TiVo2\php\php.exe D:\TiVo2\scripts\php\fromdrives.php
 D:\TiVo2\php\php.exe D:\TiVo2\scripts\php\fromdvds.php
-D:\TiVo2\php\php.exe D:\TiVo2\scripts\php\fromfolder.php
 
 ping -n 6 127.0.0.1 >nul
 
 goto loop
+
+D:\TiVo2\php\php.exe D:\TiVo2\scripts\php\fromfolder.php
