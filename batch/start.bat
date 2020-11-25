e28d404f103a2a0ed013013757e143350b138e9c
Jerry <jerrygrey@users.noreply.github.com>
2020-11-25 23:14:44 +1000

    Update start.bat

--- batch/start.bat
+++ batch/start.bat
@@ -1,6 +1,10 @@
 @echo off
 SetLocal EnableDelayedExpansion
 
-;ping -n 31 127.0.0.1 >nul
+goto :SKIP
 
-;start "TiVo2" /D "D:\TiVo2\scripts" /MAX D:\TiVo2\scripts\batch\loop.bat
+ping -n 31 127.0.0.1 >nul
+
+start "TiVo2" /D "D:\TiVo2\scripts" /MAX D:\TiVo2\scripts\batch\loop.bat
+
+:SKIP
